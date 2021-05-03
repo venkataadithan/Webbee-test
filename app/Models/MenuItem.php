ca88ac126124bc0c5ed2edf4325c139b4bb2a510
Venkat Aadithan <venkatdordie@gmail.com>
2021-05-03 14:48:04 +0530

    Menu Item with recursive children fix

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+    public function children(){
+        return $this->hasMany(MenuItem::class,'parent_id','id')->with('children');
+    }
 }
